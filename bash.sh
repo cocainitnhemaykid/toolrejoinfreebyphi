a0154f422d4bbe91e34cddebc17451de6772908e
cocainitnhemaykid <nguyentranthuytien74vn@gmail.com>
2026-07-31 16:55:41 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -11,5 +11,3 @@ yes | pkg i python-pip -y
 pip install requests rich prettytable pytz pycryptodome
 export CFLAGS="-Wno-error=implicit-function-declaration"
 pkg install python-psutil -y
-
-curl -Ls "https://raw.githubusercontent.com/cocainitnhemaykid/toolrejoinfreebyphi/refs/heads/main/phitoolrj.py" -o /sdcard/Download/phitoolrj.py
