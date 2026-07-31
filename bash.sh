da48e444978f7c9e64b76effdb14adbecd558473
cocainitnhemaykid <nguyentranthuytien74vn@gmail.com>
2026-07-31 16:40:19 +0700

    Update bash script for Termux setup and dependencies

--- bash.sh
+++ bash.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
 cd
 if [ -e "/data/data/com.termux/files/home/storage" ]; then
 	rm -rf /data/data/com.termux/files/home/storage
 fi
 termux-setup-storage
 yes | pkg update
-. <(curl https://raw.githubusercontent.com/zam2109/Toolrejoin/refs/heads/main/termux-change-repo.sh)
+. <(curl -Ls https://raw.githubusercontent.com/cocainitnhemaykid/toolrejoinfreebyphi/refs/heads/main/termux-change-repo.sh)
 yes | pkg upgrade
-yes | pkg i python
-yes | pkg i python-pip
+yes | pkg i python -y
+yes | pkg i python-pip -y
 pip install requests rich prettytable pytz pycryptodome
 export CFLAGS="-Wno-error=implicit-function-declaration"
 pkg install python-psutil -y
